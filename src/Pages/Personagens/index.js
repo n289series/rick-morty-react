febc69541721a5be827664d03fddb937b5041215
n289series <aurianacimento0@gmail.com>
2021-11-01 00:59:18 -0300

    criando arquivo de conexao para a api

--- src/Pages/Personagens/index.js
+++ src/Pages/Personagens/index.js
@@ -1,8 +1,23 @@
+import React, { useEffect, useState } from "react";
+import HeaderResponsive from "../../components/HeaderResponsive";
+import api from '../../Api';
+
 
 function Personagens(){
+    const [data, setData] = useState(null);
+    useEffect( () => {
+        async function loadAll(){
+            let res = await api.getPersonagemByName() ;
+            setData(res.data);
+            console.log(data);
+        }
+        loadAll();
+    }, [data]);
+    
+
     return (
         <div>
-            Ola
+            <HeaderResponsive/>
         </div>
     );
 }
